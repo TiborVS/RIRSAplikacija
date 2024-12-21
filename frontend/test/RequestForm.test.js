c1e9bd11117bcd8fb7ff4bd3c84c98265bbdbd67
Tibor Vito Šušnjara <tibor.susnjara@student.um.si>
2024-12-21 20:23:26 +0100

    added frontend tests for new user logic

--- frontend/test/RequestForm.test.js
+++ frontend/test/RequestForm.test.js
@@ -5,11 +5,27 @@
 import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import RequestForm from '../src/components/RequestForm'
+import userEvent from '@testing-library/user-event';
 
 global.fetch = jest.fn()
+Storage.prototype.getItem = jest.fn((key) => {
+    if (key == "token") {
+        return "abc.fake.token";
+    }
+    else if (key == "username") {
+        return "MockUsername";
+    }
+    else return undefined;
+});
+
+beforeAll(() => {
+    delete window.location;
+    window.location = { replace: jest.fn() };
+})
 
 beforeEach(() => {
     fetch.mockClear();
+    window.location.replace.mockClear();
 })
 
 test('renders form correctly', async () => {
@@ -27,14 +43,63 @@ test('renders form correctly', async () => {
     expect(screen.getByText("Create request")).toBeInTheDocument();
 })
 
-/* test('prefills existing form data when editing', async () => {
-    render(<RequestForm isEditing={true} editRequestData={
-        {
-            title: "Test Title 123",
-            description: "Some description here",
-            info: "Some info here",
-            cost: 100
+test('sends request data correctly', async () => {
+    fetch.mockResolvedValueOnce({
+        ok: true,
+        json: async () => {
+            return {}
         }
-    }/>)
-    
-}) */
+    });
+
+    const user = userEvent.setup();
+    render(<RequestForm />);
+    const textInputs = screen.queryAllByRole("textbox");
+    const titleInput = textInputs[0];
+    const descriptionInput = textInputs[1];
+    const infoInput = textInputs[2];
+    const costInput = screen.getByRole("spinbutton");
+    const buttons = screen.queryAllByRole("button");
+    const cancelButton = buttons[0]
+    const submitButton = buttons[1]
+
+    await user.type(titleInput, "Test Title");
+    await user.type(descriptionInput, "blah");
+    await user.type(infoInput, "hmm");
+    await user.type(costInput, "120");
+    await user.click(submitButton);
+
+    await new Promise(process.nextTick);
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith("http://localhost:8000/requests",{headers: {'Accept': 'application/json','Content-Type': 'application/json','Authorization': 'Bearer abc.fake.token'}, method: "POST", body: "{\"title\":\"Test Title\",\"description\":\"blah\",\"cost\":\"120\"}"});
+    expect(window.location.replace).toHaveBeenCalledTimes(1);
+    expect(window.location.replace).toHaveBeenCalledWith("/user_requests.html");
+});
+
+test('validates empty title', async () => {
+    const user = userEvent.setup();
+    render(<RequestForm />);
+
+    const buttons = screen.queryAllByRole("button");
+    const cancelButton = buttons[0]
+    const submitButton = buttons[1]
+
+    await user.click(submitButton);
+    expect(screen.getByText("Title cannot be empty!")).toBeInTheDocument();
+});
+
+test('validates empty description', async () => {
+    const user = userEvent.setup();
+    render(<RequestForm />);
+
+    const textInputs = screen.queryAllByRole("textbox");
+    const titleInput = textInputs[0];
+    const buttons = screen.queryAllByRole("button");
+    const cancelButton = buttons[0]
+    const submitButton = buttons[1]
+
+    await user.type(titleInput, "Test Title");
+    await user.click(submitButton);
+
+    expect(screen.getByText("Description cannot be empty!")).toBeInTheDocument();
+});
